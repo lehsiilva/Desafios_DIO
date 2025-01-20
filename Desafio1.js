87bd43d766c0a0a5760d403dc1fbca2f6784029f
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-01-20 02:58:29 -0300

    Desafio Completo

--- Desafio1.js
+++ Desafio1.js
@@ -1,1 +1,69 @@
-console.log("Seja-Bem Vindo")
+/*# 1️⃣ Desafio Classificador de nível de Herói
+
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 5.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000= Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói de nome **{nome}** está no nível de **{nivel}**" */
+
+const prompt = require('prompt-sync')(); //biblioteca para rodar o prompt
+
+let qntHerois = parseInt(prompt("Digite a quantidade de heróis: ")) //parseInt = transformar o dado em dado numerico
+
+
+for(let i = 0; i < qntHerois; i++) {
+
+let nomeHeroi = prompt("Digite o nome do seu herói: ")
+let xp = parseInt(prompt("Digite a XP do seu herói: "))
+
+let nivel
+
+if(xp < 1000){
+    nivel = "Ferro"
+} else if(xp >= 1001 && xp <= 2000 ){
+    nivel = "Bronze"
+} else if(xp >= 2001 && xp <= 5000 ){
+    nivel = "Prata"
+} else if(xp >= 5001 && xp <= 7000 ){
+    nivel = "Ouro"
+} else if(xp >= 7001 && xp <= 8000 ){
+    nivel = "Platina"
+} else if(xp >= 8001 && xp <= 9000 ){
+    nivel = "Ascendente"
+} else if(xp >= 9001 && xp <= 10000 ){
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeHeroi + " "+ "está no nível de " + nivel)
+
+}
+
+
+
+
+
+
+
+
+
