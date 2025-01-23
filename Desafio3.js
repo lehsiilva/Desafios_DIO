141794fdfca29b197092d52caa1b2a8e4bf4a7d0
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-01-23 19:03:31 -0300

    Desafio Completo

---
+++ Desafio3.js
@@ -0,0 +1,71 @@
+/*# 3️⃣ Escrevendo as classes de um Jogo
+
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Estruturas de decisões
+- Funções
+- Classes e Objetos
+
+## Objetivo:
+
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada */
+
+  class aventura {
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+        ataque(){
+            let ataque;
+
+            if (this.tipo === "mago"){
+                ataque = "magia";
+            } else if (this.tipo === "guerreiro"){
+                ataque = "espada";
+            }else if (this.tipo === "monge"){
+                ataque = "artes marciais";
+            }else if (this.tipo === "ninja") {
+                ataque = "shuriken";
+            }else{
+                ataque = "desconhecido";
+            }
+        
+        console.log( "O " + this.tipo + " atacou usando " + ataque);
+    }
+        
+  }
+
+  let nome = "leticia";
+  let idade = 15;
+  let tipo = "mago";
+
+  let HeroiAventureiro = new aventura(nome, idade, tipo);
+  
+  HeroiAventureiro.ataque();
